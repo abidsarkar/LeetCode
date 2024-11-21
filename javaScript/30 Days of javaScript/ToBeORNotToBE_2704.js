3f9ade502f11294954f1b367d68661c66d5d3062
abidsarkar <abidsarkar0@gmail.com>
2024-11-21 17:08:07 +0600

    updated

--- javaScript/30 Days of javaScript/ToBeORNotToBE_2704.js
+++ javaScript/30 Days of javaScript/ToBeORNotToBE_2704.js
@@ -1,30 +1,52 @@
+// https://leetcode.com/problems/to-be-or-not-to-be/?envType=study-plan-v2&envId=30-days-of-javascript
+//function under a function and return the error
+
 /**
  * @param {string} val
  * @return {Object}
  */
-var expect = function(val) {
-   function toBe(val2) {
-        if(val ===  val2){
-            return true;
-        }else{
-            throw new Error('Not Equal');
-        }
-    }
-    function notToBe(val3) {
-        if(val !== val3){
-            return true;
-        }
-        else{
-        throw new Error('Equal')
-        }
-    }
-    return {
-        toBe,
-        notToBe
-    }
-};
+// var expect = function(val) {
+//    function toBe(val2) {
+//         if(val ===  val2){
+//             return true;
+//         }else{
+//             throw new Error('Not Equal');
+//         }
+//     }
+//     function notToBe(val3) {
+//         if(val !== val3){
+//             return true;
+//         }
+//         else{
+//         throw new Error('Equal')
+//         }
+//     }
+//     return {
+//         toBe,
+//         notToBe
+//     }
+// };
 
+let expect = function(val1){
+    return{
+        toBe: (val2) =>{
+            if(val1 === val2){
+                return true;
+            }else{
+                throw new Error("Not Equal");
+            }
+        },
+        notToBe: (val2) =>{
+            if(val1 === val2){
+                throw new Error("Equal");
+            }
+            else{
+                return true;
+            }
+        }
+    }
+}
 console.log(expect(5).toBe(5)); // true
-console.log(expect(5).notToBe(5));// throws "Equal"
+console.log(expect(5).notToBe(6));// throws "Equal"
 
  
