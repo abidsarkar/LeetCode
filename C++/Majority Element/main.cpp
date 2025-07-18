574fb6fa779c5a7f2d73c67816decbfeb425e5b0
abidsarkar <abidsarkar0@gmail.com>
2025-07-18 16:49:17 +0600

    new problem solve

---
+++ C++/Majority Element/main.cpp
@@ -0,0 +1,35 @@
+//https://leetcode.com/problems/majority-element/description/
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int majorityElement(vector<int> &nums) {
+    int n = nums.size();
+    if(n==1){
+        return nums[0];
+    }
+    sort(nums.begin(), nums.end());
+    int freq = 1, ans = nums[0];
+    for (int i = 1; i < n; ++i) {
+        if(nums[i] == nums[i-1]){
+            freq++;
+        } else{
+            freq=1;
+            ans = nums[i];
+        }
+        if(freq>n/2){
+            return ans;
+        }
+    }
+    return -1;
+}
+
+int main() {
+    vector<int> vector1 = {3, 2, 3};
+    int ans = majorityElement(vector1);
+    cout << ans;
+    return 0;
+}
